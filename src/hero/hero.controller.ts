a52195b78ce14170d68ec3ab0be513844093cd55
Tsioryfaniry <andrianarivonyfaniry@gmail.com>
2023-12-12 14:29:52 +0300

    first commit

--- src/hero/hero.controller.ts
+++ src/hero/hero.controller.ts
@@ -6,7 +6,7 @@ export class HeroController {
     constructor(private heroService:HeroService){}
     @Post()
     async postHero(@Body() body){
-        console.log(body)
+        // console.log(body)
         return await this.heroService.createOrUpdateHero(body.title, body.image).then(data=>data) 
     }
     @Get()
